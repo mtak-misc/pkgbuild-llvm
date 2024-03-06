befb8462667a187afc2908b68239fe1309a98805
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-03-07 06:53:26 +0900

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+USERID=$1
+
+pacman -Syu --noconfirm base-devel sudo git
+useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+#chown builder -R ..
+#chown builder -R /tmp
+cd ./llvm-git ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck -sc"
