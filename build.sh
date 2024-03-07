f2aef93538ef3752ac1a921c8856a456d801212e
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-03-07 21:23:50 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ pacman -Syu --noconfirm base-devel sudo git
 useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 #chown builder -R ..
 #chown builder -R /tmp
-cd ./llvm-git ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck -sc" > /dev/null
+cd ./llvm-git ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck -sc"
