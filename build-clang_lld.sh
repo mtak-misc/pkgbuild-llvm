59391fa82ab3ba28fd7de9e71aadefa8773e24a7
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-03-08 21:19:18 +0900

    Update build-clang_lld.sh

--- build-clang_lld.sh
+++ build-clang_lld.sh
@@ -8,3 +8,6 @@ useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: AL
 #chown builder -R ..
 #chown builder -R /tmp
 cd ./compiler-rt ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
+rm *debug*.zst
+pacman -U --noconfirm compiler-rt-*.zst
+cd ./clang ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
