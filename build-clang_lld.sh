51231832741cd953054077ec7a16cc7007fdb48a
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-03-08 20:54:02 +0900

    Update build-clang_lld.sh

--- build-clang_lld.sh
+++ build-clang_lld.sh
@@ -2,8 +2,8 @@
 USERID=$1
 
 rm *debug*.zst
-pacman -Uy --noconfirm llvm-*.zst
-#pacman -Syu --noconfirm base-devel sudo git python-myst-parser
+pacman -Syu --noconfirm base-devel sudo git python-myst-parser
+pacman -U --noconfirm llvm-*.zst
 useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 #chown builder -R ..
 #chown builder -R /tmp
