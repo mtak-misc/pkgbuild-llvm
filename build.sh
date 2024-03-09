66f33045165216e4b62693fc4f319dba96e52779
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-03-09 10:07:04 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,18 @@ USERID=$1
 
 pacman -Syu --noconfirm base-devel sudo git python-myst-parser
 useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-#chown builder -R ..
-#chown builder -R /tmp
-cd ./llvm-git ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
+cd ./llvm ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
+rm llvm-debug*.zst
+pacman -Syu --noconfirm base-devel sudo git python-myst-parser
+pacman -U --noconfirm llvm-*.zst
+useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+cd ../compiler-rt ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
+rm compiler-rt-debug*.zst
+pacman -U --noconfirm compiler-rt-*.zst
+cd ../clang ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
+rm clang-debug*.zst
+pacman -U --noconfirm clang-*.zst
+cd ../lld ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
+rm lld-debug*.zst
+cd ..
+mv */*.zst .
