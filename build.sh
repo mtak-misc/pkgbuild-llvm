a26f45435664ced69295ae8fdd39dddbd0763aed
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-03-09 17:27:31 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,18 +3,21 @@ USERID=$1
 
 pacman -Syu --noconfirm base-devel sudo git python-myst-parser
 useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
 cd ./llvm ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
 rm llvm-debug*.zst
-pacman -Syu --noconfirm base-devel sudo git python-myst-parser
 pacman -U --noconfirm llvm-*.zst
 
 cd ../compiler-rt ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
 rm compiler-rt-debug*.zst
 pacman -U --noconfirm compiler-rt-*.zst
+
 cd ../clang ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
 rm clang-debug*.zst
 pacman -U --noconfirm clang-*.zst
+
 cd ../lld ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
 rm lld-debug*.zst
+
 cd ..
 mv */*.zst .
