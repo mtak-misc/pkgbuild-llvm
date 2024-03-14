f70f4a5ecccb8adaf13bd0535b255a21816740f0
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-03-14 20:56:41 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@ USERID=$1
 pacman -Syu --noconfirm base-devel sudo git python-myst-parser
 useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-ls -l *.pkg.tar.zst
-find ./ -maxdepth 1 -name "*.pkg.tar.zst"
 RES=`find ./ -maxdepth 1 -name "*.pkg.tar.zst" 2>/dev/null`
 if [ $? -ne 0 ]; then
   echo 'Unexpected error.'
