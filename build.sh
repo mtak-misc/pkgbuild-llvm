392e72bd42850d8dfa96594c20143e225e4ea7ae
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-03-14 12:04:35 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,16 @@ USERID=$1
 pacman -Syu --noconfirm base-devel sudo git python-myst-parser
 useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-pacman --noconfirm -U *.pkg.tar.zst
-export CC=clang
-export CXX=clang++
+RES=`find ./ -maxdepth 1 -name *.pkg.tar.zst 2>/dev/null`
+if [ $? -ne 0 ]; then
+  #
+elif [ -z "$RES" ]; then
+  #
+else
+  pacman --noconfirm -U *.pkg.tar.zst
+  export CC=clang
+  export CXX=clang++
+fi
 
 cd ./llvm ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
 rm llvm-debug*.zst
