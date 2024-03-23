bb47546234431154b1b4e93cc6b9f5c1a5bcfceb
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-03-23 11:15:11 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,9 +19,9 @@ cd ./llvm ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfir
 rm llvm-debug*.zst
 pacman -U --noconfirm llvm-*.zst
 
-#cd ../compiler-rt ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
-#rm compiler-rt-debug*.zst
-#pacman -U --noconfirm compiler-rt-*.zst
+cd ../compiler-rt ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
+rm compiler-rt-debug*.zst
+pacman -U --noconfirm compiler-rt-*.zst
 
 #cd ../clang ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
 #rm clang-debug*.zst
@@ -30,5 +30,5 @@ pacman -U --noconfirm llvm-*.zst
 #cd ../lld ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
 #rm lld-debug*.zst
 
-#cd ..
+cd ..
 mv */*.zst .
