4851364eb482ed971f23fe952c9852901a2644ff
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-03-23 21:30:08 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,19 +15,19 @@ else
   export CXX=clang++
 fi
 
-cd ./llvm ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
-rm llvm-debug*.zst
-pacman -U --noconfirm llvm-*.zst
+#cd ./llvm ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
+#rm llvm-debug*.zst
+#pacman -U --noconfirm llvm-*.zst
 
-cd ../compiler-rt ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
-rm compiler-rt-debug*.zst
-pacman -U --noconfirm compiler-rt-*.zst
+#cd ../compiler-rt ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
+#rm compiler-rt-debug*.zst
+#pacman -U --noconfirm compiler-rt-*.zst
 
-cd ../clang ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
-rm clang-debug*.zst
-pacman -U --noconfirm clang-*.zst
+#cd ../clang ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
+#rm clang-debug*.zst
+#pacman -U --noconfirm clang-*.zst
 
-cd ../lld ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
+cd ./lld ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
 rm lld-debug*.zst
 
 cd ..
