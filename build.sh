45b0b397d8ea372b1a4f2fc7e5c0b3a884200c21
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-04-09 19:32:10 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,33 +8,34 @@ RES=`find ./ -maxdepth 1 -name "*.pkg.tar.zst" 2>/dev/null`
 if [ $? -ne 0 ]; then
   echo 'Unexpected error.'
 elif [ -z "$RES" ]; then
-  pacman -S --noconfirm llvm llvm-libs compiler-rt clang lld
+  echo 'No packages.'
 else
   pacman --noconfirm -U *.pkg.tar.zst
+  export CC=clang
+  export CXX=clang++
 fi
-
-export CC=clang
-export CXX=clang++
 
 cd ./llvm ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
 rm llvm-debug*.zst
+#pacman -U --noconfirm llvm-*.zst
 mv llvm-*.zst ..
 cd ..
-rm -rf llvm
+#rm -rf llvm
 
 cd ./compiler-rt ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
 rm compiler-rt-debug*.zst
+#pacman -U --noconfirm compiler-rt-*.zst
 mv compiler-rt-*.zst ..
 cd ..
-rm -rf compiler-rt
+#rm -rf compiler-rt
 
 cd ./clang ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
 rm clang-debug*.zst
+#pacman -U --noconfirm clang-*.zst
 mv clang-*.zst ..
 cd ..
-rm -rf clang
+#rm -rf clang
 
 cd ./lld ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
 rm lld-debug*.zst
 mv lld-*.zst ..
-
