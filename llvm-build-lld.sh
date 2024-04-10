c18e85c9992f035b64feed3e00d6cd7798973f9c
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-04-10 09:44:21 +0900

    Create llvm-build-lld.sh

---
+++ llvm-build-lld.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+USERID=$1
+
+pacman -Syu --noconfirm base-devel sudo git python-myst-parser
+useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+pacman --noconfirm -U *.pkg.tar.zst
+export CC=clang
+export CXX=clang++
+
+cd ./lld ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
+rm lld-debug*.zst
+mv lld-*.zst ..
