38168512357e03499c6c1e63313b2e214d82b51c
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-04-10 09:41:49 +0900

    Create llvm-build-compiler-rt.sh

---
+++ llvm-build-compiler-rt.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+USERID=$1
+
+pacman -Syu --noconfirm base-devel sudo git python-myst-parser
+useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+RES=`find ./ -maxdepth 1 -name "*.pkg.tar.zst" 2>/dev/null`
+if [ $? -ne 0 ]; then
+  echo 'Unexpected error.'
+elif [ -z "$RES" ]; then
+  echo 'No packages.'
+else
+  pacman --noconfirm -U *.pkg.tar.zst
+  export CC=clang
+  export CXX=clang++
+fi
+
+cd ./compiler-rt ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
+rm compiler-rt-debug*.zst
+mv compiler-rt-*.zst ..
