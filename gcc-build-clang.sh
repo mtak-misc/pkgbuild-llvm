99ba0524ffaf4a18b564112164d4ef99a9b54847
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-04-10 11:44:56 +0900

    Update gcc-build-clang.sh

--- gcc-build-clang.sh
+++ gcc-build-clang.sh
@@ -5,6 +5,7 @@ pacman -Syu --noconfirm base-devel sudo git python-myst-parser
 useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 pacman --noconfirm -U llvm-*.zst compiler-rt-*.zst
+rm llvm-*.zst compiler-rt-*.zst
 
 cd ./clang ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
 rm clang-debug*.zst
