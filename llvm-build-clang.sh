10d89fb73cb88660ff18e6ad69888f211a0f971a
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-04-10 09:45:17 +0900

    Update llvm-build-clang.sh

--- llvm-build-clang.sh
+++ llvm-build-clang.sh
@@ -4,18 +4,10 @@ USERID=$1
 pacman -Syu --noconfirm base-devel sudo git python-myst-parser
 useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
-RES=`find ./ -maxdepth 1 -name "*.pkg.tar.zst" 2>/dev/null`
-if [ $? -ne 0 ]; then
-  echo 'Unexpected error.'
-elif [ -z "$RES" ]; then
-  echo 'No packages.'
-else
-  pacman --noconfirm -U *.pkg.tar.zst
-  export CC=clang
-  export CXX=clang++
-fi
+pacman --noconfirm -U *.pkg.tar.zst
+export CC=clang
+export CXX=clang++
 
 cd ./clang ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
 rm clang-debug*.zst
-pacman -U --noconfirm clang-*.zst
 mv clang-*.zst ..
