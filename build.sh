d5789c5a1eb2d7f8fec4061e25a1764f1a4b816e
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-04-11 21:27:45 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ else
   export CXX=clang++
 fi
 
-cd ./llvm ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
+cd ./llvm ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" nice -n 10 makepkg --noconfirm --nocheck --skippgpcheck -sc"
 rm llvm-debug*.zst
 pacman -U --noconfirm llvm-*.zst
 mv llvm-*.zst ..
