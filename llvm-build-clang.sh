d5242dd44e92d9b3282aefe5eb6376183cf0f2c6
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-04-19 09:35:52 +0900

    Update llvm-build-clang.sh

--- llvm-build-clang.sh
+++ llvm-build-clang.sh
@@ -4,6 +4,8 @@ USERID=$1
 pacman -Syu --noconfirm base-devel sudo git python-myst-parser
 useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
+pacman -S --noconfirm graphviz
+
 pacman --noconfirm -U *.pkg.tar.zst
 export CC=clang
 export CXX=clang++
