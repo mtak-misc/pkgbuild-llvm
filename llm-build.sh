128ef285dc7e5c91f1d0e857b9785caa8702f922
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-04-19 11:01:19 +0900

    Update llm-build.sh

--- llm-build.sh
+++ llm-build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 USERID=$1
 
-pacman -Syu --noconfirm base-devel sudo git python-myst-parser
+pacman -Syu --noconfirm base-devel sudo git python-myst-parser graphviz
 useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 pacman -S --noconfirm llvm compiler-rt clang lld
