c20d5bf3e852579fc997dddf32e0a8311559601c
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-06-07 07:46:01 +0900

    Update modify-lld-pkgbuild.sh

--- modify-lld-pkgbuild.sh
+++ modify-lld-pkgbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -i 's/pkgver=17.0.6/pkgver=18.1.6/g' lld/PKGBUILD
+sed -i 's/pkgver=17.0.6/pkgver=18.1.7/g' lld/PKGBUILD
 sed -i 's/4ac13125616dc44905b85820aa403d27ec1226329b7f674daeb5f5584c6f0b22/SKIP/g' lld/PKGBUILD
 sed -i 's/b638167da139126ca11917b6880207cc6e8f9d1cbb1a48d87d017f697ef78188/SKIP/g' lld/PKGBUILD
 sed -i 's/9e7535a353aa862730b4ba38df42e06f6856b40c4cc51b57f27b5046dc21d70d/SKIP/g' lld/PKGBUILD
