d1a995f8f8b8f10464272e5724b9b89826b44a19
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-06-16 16:31:49 +0900

    Update modify-llvm-pkgbuild.sh

--- modify-llvm-pkgbuild.sh
+++ modify-llvm-pkgbuild.sh
@@ -6,4 +6,5 @@ sed -i 's/b638167da139126ca11917b6880207cc6e8f9d1cbb1a48d87d017f697ef78188/SKIP/
 sed -i 's/807f069c54dc20cb47b21c1f6acafdd9c649f3ae015609040d6182cab01140f4/SKIP/g' llvm/PKGBUILD
 sed -i 's/3054d0a9c9375dab1a4539cc2cc45ab340341c5d71475f9599ba7752e222947b/SKIP/g' llvm/PKGBUILD
 sed -i '/mkdir build/i \ \ curl -L https://github.com/llvm/llvm-project/commit/f800c1f3b207e7bcdc8b4c7192928d9a078242a0.patch -o ../llvm.patch' llvm/PKGBUILD 
+sed -i '/mkdir build/i \ \ ln -s llvm-18.1.7.src llvm' llvm/PKGBUILD
 sed -i '/mkdir build/i \ \ cd .. ; patch -Np1 -i llvm.patch' llvm/PKGBUILD
