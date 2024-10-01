5b9b869b2be8f7e164b9e89e08c255fed8487e6e
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-10-01 13:15:07 +0900

    Update modify-lld-pkgbuild.sh

--- modify-lld-pkgbuild.sh
+++ modify-lld-pkgbuild.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-sed -i 's/pkgver=17.0.6/pkgver=18.1.8/g' lld/PKGBUILD
-sed -i 's/4ac13125616dc44905b85820aa403d27ec1226329b7f674daeb5f5584c6f0b22/SKIP/g' lld/PKGBUILD
-sed -i 's/b638167da139126ca11917b6880207cc6e8f9d1cbb1a48d87d017f697ef78188/SKIP/g' lld/PKGBUILD
-sed -i 's/9e7535a353aa862730b4ba38df42e06f6856b40c4cc51b57f27b5046dc21d70d/SKIP/g' lld/PKGBUILD
-sed -i 's/807f069c54dc20cb47b21c1f6acafdd9c649f3ae015609040d6182cab01140f4/SKIP/g' lld/PKGBUILD
+sed -i 's/pkgver=18.1.0/pkgver=19.1.0/g' lld/PKGBUILD
+sed -i 's/800e6805fb613eb8428f7a199f6d06c4c19f78f2db69157a129bb04fc1c99769/SKIP/g' lld/PKGBUILD
+sed -i 's/f68cf90f369bc7d0158ba70d860b0cb34dbc163d6ff0ebc6cfa5e515b9b2e28d/SKIP/g' lld/PKGBUILD
+sed -i 's/c31577d16978b0da0e472ef751f74893a5b459a7ea4a383b75f7ab93cf1e6877/SKIP/g' lld/PKGBUILD
+sed -i 's/59badef592dd34893cd319d42b323aaa990b452d05c7180ff20f23ab1b41e837/SKIP/g' lld/PKGBUILD
 sed -i '/patch -Np2/d' lld/PKGBUILD
-sed -i 's/76f0625c86638d0fa06b54f1b151a47d8c71d72466da49557d288e33de89c288/SKIP/g' lld/PKGBUILD
+# sed -i 's//SKIP/g' lld/PKGBUILD
