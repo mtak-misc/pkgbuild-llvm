0e29ae4635c141a4f66d47874ee8bceec79eddb6
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-12-18 21:44:34 +0900

    Update modify-llvm-pkgbuild.sh

--- modify-llvm-pkgbuild.sh
+++ modify-llvm-pkgbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -i 's/pkgver=18.1.8/pkgver=19.1.5/g' llvm/PKGBUILD
+sed -i 's/pkgver=18.1.8/pkgver=19.1.6/g' llvm/PKGBUILD
 sed -i 's/pkgrel=5/pkgrel=1/g' llvm/PKGBUILD
 sed -i 's/f68cf90f369bc7d0158ba70d860b0cb34dbc163d6ff0ebc6cfa5e515b9b2e28d/SKIP/g' llvm/PKGBUILD
 sed -i 's/59badef592dd34893cd319d42b323aaa990b452d05c7180ff20f23ab1b41e837/SKIP/g' llvm/PKGBUILD
