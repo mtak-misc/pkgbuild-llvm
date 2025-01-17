b7ec5ad6e30ae7a465c38e83d182145c9745fb06
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2025-01-18 08:03:47 +0900

    Update modify-compiler-rt-pkgbuild.sh

--- modify-compiler-rt-pkgbuild.sh
+++ modify-compiler-rt-pkgbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -i 's/pkgver=18.1.8/pkgver=19.1.6/g' compiler-rt/PKGBUILD
+sed -i 's/pkgver=19.1.6/pkgver=19.1.7/g' compiler-rt/PKGBUILD
 # sed -i 's/pkgrel=2/pkgrel=1/g' compiler-rt/PKGBUILD
-sed -i 's/e054e99a9c9240720616e927cb52363abbc8b4f1ef0286bad3df79ec8fdf892f/SKIP/g' compiler-rt/PKGBUILD
-sed -i 's/59badef592dd34893cd319d42b323aaa990b452d05c7180ff20f23ab1b41e837/SKIP/g' compiler-rt/PKGBUILD
+sed -i 's/0d4f312e1419152282c267e6b6a1fa5914a7a0c753a5e926bee1c8c28e614ae4/SKIP/g' compiler-rt/PKGBUILD
+sed -i 's/9c7ec82d9a240dc2287b8de89d6881bb64ceea0dcd6ce133c34ef65bda22d99e/SKIP/g' compiler-rt/PKGBUILD
