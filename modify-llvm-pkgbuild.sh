09d966911fdf1e0920d8584d2d2d16c4ad33b99c
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2025-01-19 01:05:26 +0900

    Update modify-llvm-pkgbuild.sh

--- modify-llvm-pkgbuild.sh
+++ modify-llvm-pkgbuild.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sed -i 's/pkgver=19.1.6/pkgver=19.1.7/g' llvm/PKGBUILD
-sed -i 's/pkgrel=3/pkgrel=1/g' llvm/PKGBUILD
-sed -i 's/ad1a3b125ff014ded290094088de40efb9193ce81a24278184230b7d401f8a3e/SKIP/g' llvm/PKGBUILD
-sed -i 's/9c7ec82d9a240dc2287b8de89d6881bb64ceea0dcd6ce133c34ef65bda22d99e/SKIP/g' llvm/PKGBUILD
-sed -i 's/0e8048333bab2ba3607910e5d074259f08dccf00615778d03a2a55416718eb45/SKIP/g' llvm/PKGBUILD
+#sed -i 's/pkgver=19.1.6/pkgver=19.1.7/g' llvm/PKGBUILD
+#sed -i 's/pkgrel=3/pkgrel=1/g' llvm/PKGBUILD
+#sed -i 's/ad1a3b125ff014ded290094088de40efb9193ce81a24278184230b7d401f8a3e/SKIP/g' llvm/PKGBUILD
+#sed -i 's/9c7ec82d9a240dc2287b8de89d6881bb64ceea0dcd6ce133c34ef65bda22d99e/SKIP/g' llvm/PKGBUILD
+#sed -i 's/0e8048333bab2ba3607910e5d074259f08dccf00615778d03a2a55416718eb45/SKIP/g' llvm/PKGBUILD
 #sed -i '/patch -Np2/d' llvm/PKGBUILD
