59fba1431eabcb56028be97be373d332d59d79be
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2025-06-13 21:40:00 +0900

    Update modify-compiler-rt-pkgbuild.sh

--- modify-compiler-rt-pkgbuild.sh
+++ modify-compiler-rt-pkgbuild.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+NEWVERSION=$1
+PKGVERSION=$(grep "pkgver=" compiler-rt/PKGBUILD)
+OLDVERSION="${PKGVERSION#pkgver=}"
+PKGREL=$(grep "pkgrel=" PKGBUILD)
 
-sed -i 's/pkgver=19.1.7/pkgver=20.1.6/g' compiler-rt/PKGBUILD
-# sed -i 's/pkgrel=2/pkgrel=1/g' compiler-rt/PKGBUILD
-sed -i 's/c12b6e764202c615c1a3af9a13d477846878757ae0e29e5f8979215a6958fffc/SKIP/g' compiler-rt/PKGBUILD
-sed -i 's/11c5a28f90053b0c43d0dec3d0ad579347fc277199c005206b963c19aae514e3/SKIP/g' compiler-rt/PKGBUILD
+echo "sed -i 's/${OLDVERSION}/${NEWVERSION}/g' compiler-rt/PKGBUILD" | bash
+echo "sed -i 's/${PKGREL}/pkgrel=1/g' compiler-rtPKGBUILD" | bash
+grep -oP "'\K[a-f0-9]{64}(?='.*)" compiler-rt/PKGBUILD | awk '{print "sed -i \x27s/"$1"/SKIP/g\x27 compiler-rt/PKGBUILD"}' | bash
