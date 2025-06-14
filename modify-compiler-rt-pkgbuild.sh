0642fd8eb08fd7545b54caad34d5c234b652bab7
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2025-06-14 13:52:11 +0900

    Update modify-compiler-rt-pkgbuild.sh

--- modify-compiler-rt-pkgbuild.sh
+++ modify-compiler-rt-pkgbuild.sh
@@ -5,5 +5,5 @@ OLDVERSION="${PKGVERSION#pkgver=}"
 PKGREL=$(grep "pkgrel=" PKGBUILD)
 
 echo "sed -i 's/${OLDVERSION}/${NEWVERSION}/g' compiler-rt/PKGBUILD" | bash
-echo "sed -i 's/${PKGREL}/pkgrel=1/g' compiler-rtPKGBUILD" | bash
+echo "sed -i 's/${PKGREL}/pkgrel=1/g' compiler-rt/PKGBUILD" | bash
 grep -oP "'\K[a-f0-9]{64}(?='.*)" compiler-rt/PKGBUILD | awk '{print "sed -i \x27s/"$1"/SKIP/g\x27 compiler-rt/PKGBUILD"}' | bash
