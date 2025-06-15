82b3160b364d774e62cda04451d0b5d9406bed73
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2025-06-15 16:56:19 +0900

    Update modify-lld-pkgbuild.sh

--- modify-lld-pkgbuild.sh
+++ modify-lld-pkgbuild.sh
@@ -4,7 +4,7 @@ PKGVERSION=$(grep "pkgver=" lld/PKGBUILD)
 OLDVERSION="${PKGVERSION#pkgver=}"
 PKGREL=$(grep "pkgrel=" lld/PKGBUILD)
 
-sed -i 's/gcc-libs/llvm-libs/g' lld/PKGBUILD
+sed -i "s/'llvm-libs' 'gcc-libs'/'llvm-libs'/g" lld/PKGBUILD
 echo "sed -i 's/${OLDVERSION}/${NEWVERSION}/g' lld/PKGBUILD" | bash
 echo "sed -i 's/${PKGREL}/pkgrel=1/g' lld/PKGBUILD" | bash
 grep -oP "'\K[a-f0-9]{64}(?='.*)" lld/PKGBUILD | awk '{print "sed -i \x27s/"$1"/SKIP/g\x27 lld/PKGBUILD"}' | bash
