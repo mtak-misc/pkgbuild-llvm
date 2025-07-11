6e6f4b8b4c78cf7d86f2d48ccde51735d3a07d82
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2025-07-11 13:52:51 +0900

    Update modify-compiler-rt-pkgbuild.sh

--- modify-compiler-rt-pkgbuild.sh
+++ modify-compiler-rt-pkgbuild.sh
@@ -5,7 +5,6 @@ OLDVERSION="${PKGVERSION#pkgver=}"
 PKGREL=$(grep "pkgrel=" compiler-rt/PKGBUILD)
 
 sed -i "s/'gcc-libs/'llvm-libs/g" compiler-rt/PKGBUILD
-cat compiler-rt/PKGBUILD
 echo "sed -i 's/${OLDVERSION}/${NEWVERSION}/g' compiler-rt/PKGBUILD" | bash
 echo "sed -i 's/${PKGREL}/pkgrel=1/g' compiler-rt/PKGBUILD" | bash
 grep -oP "'\K[a-f0-9]{64}(?='.*)" compiler-rt/PKGBUILD | awk '{print "sed -i \x27s/"$1"/SKIP/g\x27 compiler-rt/PKGBUILD"}' | bash
