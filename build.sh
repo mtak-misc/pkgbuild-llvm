d4db75295aa80131e584ebb1e46739ef8dc3aa79
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2025-07-11 13:33:24 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ export CXX=clang++
 #mv lld-*.zst ..
 #cd ..
 
-#cd ./compiler-rt ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
+cd ./compiler-rt ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
 #rm compiler-rt-debug*.zst
 #pacman -U --noconfirm compiler-rt-*.zst
 #mv compiler-rt-*.zst ..
