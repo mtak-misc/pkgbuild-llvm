a2edb9d9f4ac431f4b40920f7fdc125bc0fd7c2e
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2025-07-11 13:18:45 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -30,11 +30,11 @@ export CXX=clang++
 #mv lld-*.zst ..
 #cd ..
 
-cd ./compiler-rt ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
-rm compiler-rt-debug*.zst
-pacman -U --noconfirm compiler-rt-*.zst
-mv compiler-rt-*.zst ..
-cd ..
+#cd ./compiler-rt ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
+#rm compiler-rt-debug*.zst
+#pacman -U --noconfirm compiler-rt-*.zst
+#mv compiler-rt-*.zst ..
+#cd ..
 
 #cd ./clang ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
 #rm clang-debug*.zst
