0688433f99926973931c0cbc2bb7a35d040f6d41
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2025-08-30 10:34:21 +0900

    Update modify-compiler-rt-pkgbuild.sh

--- modify-compiler-rt-pkgbuild.sh
+++ modify-compiler-rt-pkgbuild.sh
@@ -4,6 +4,15 @@ PKGVERSION=$(grep "pkgver=" compiler-rt/PKGBUILD)
 OLDVERSION="${PKGVERSION#pkgver=}"
 PKGREL=$(grep "pkgrel=" compiler-rt/PKGBUILD)
 
+RES=`find ./ -maxdepth 1 -name "*.pkg.tar.zst" 2>/dev/null`
+if [ $? -ne 0 ]; then
+  echo 'Unexpected error.'
+elif [ -z "$RES" ]; then
+  echo 'No packages.'
+#  pacman --noconfirm -S llvm compiler-rt clang lld
+else
+  sed -i "s/'gcc-libs/'llvm-libs/g" compiler-rt/PKGBUILD
+fi
 #sed -i "s/'gcc-libs/'llvm-libs/g" compiler-rt/PKGBUILD
 sed -i '/makedepends_x86_64/d' compiler-rt/PKGBUILD
 echo "sed -i 's/${OLDVERSION}/${NEWVERSION}/g' compiler-rt/PKGBUILD" | bash
