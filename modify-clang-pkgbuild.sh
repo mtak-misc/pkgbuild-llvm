fb349f0d62288bc92803c90465b1d94289771007
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2025-08-30 10:35:59 +0900

    Update modify-clang-pkgbuild.sh

--- modify-clang-pkgbuild.sh
+++ modify-clang-pkgbuild.sh
@@ -4,6 +4,16 @@ PKGVERSION=$(grep "pkgver=" clang/PKGBUILD)
 OLDVERSION="${PKGVERSION#pkgver=}"
 PKGREL=$(grep "pkgrel=" clang/PKGBUILD)
 
+RES=`find ./ -maxdepth 1 -name "*.pkg.tar.zst" 2>/dev/null`
+if [ $? -ne 0 ]; then
+  echo 'Unexpected error.'
+elif [ -z "$RES" ]; then
+  echo 'No packages.'
+#  pacman --noconfirm -S llvm compiler-rt clang lld
+else
+  sed -i 's/gcc/clang/g' clang/PKGBUILD
+fi
+
 #sed -i 's/gcc/clang/g' clang/PKGBUILD
 echo "sed -i 's/${OLDVERSION}/${NEWVERSION}/g' clang/PKGBUILD" | bash
 echo "sed -i 's/${PKGREL}/pkgrel=1/g' clang/PKGBUILD" | bash
