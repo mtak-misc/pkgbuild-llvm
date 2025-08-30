df5550461ad36dfff206b08956a1353190d93751
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2025-08-30 10:33:38 +0900

    Update modify-llvm-pkgbuild.sh

--- modify-llvm-pkgbuild.sh
+++ modify-llvm-pkgbuild.sh
@@ -4,6 +4,15 @@ PKGVERSION=$(grep "pkgver=" llvm/PKGBUILD)
 OLDVERSION="${PKGVERSION#pkgver=}"
 PKGREL=$(grep "pkgrel=" llvm/PKGBUILD)
 
+RES=`find ./ -maxdepth 1 -name "*.pkg.tar.zst" 2>/dev/null`
+if [ $? -ne 0 ]; then
+  echo 'Unexpected error.'
+elif [ -z "$RES" ]; then
+  echo 'No packages.'
+#  pacman --noconfirm -S llvm compiler-rt clang lld
+else
+  sed -i 's/gcc-libs/llvm-libs/g' llvm/PKGBUILD
+fi
 #sed -i 's/gcc-libs/llvm-libs/g' llvm/PKGBUILD
 echo "sed -i 's/${OLDVERSION}/${NEWVERSION}/g' llvm/PKGBUILD" | bash
 echo "sed -i 's/${PKGREL}/pkgrel=1/g' llvm/PKGBUILD" | bash
