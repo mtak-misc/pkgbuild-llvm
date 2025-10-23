3c34fb187e1e92b93932075e57d8c9bd6ddae975
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2025-10-23 19:58:02 +0900

    Comment out patch removal in modify-clang-pkgbuild.sh
    Comment out the line that removes the patch command from PKGBUILD.

--- modify-clang-pkgbuild.sh
+++ modify-clang-pkgbuild.sh
@@ -19,6 +19,6 @@ echo "sed -i 's/${OLDVERSION}/${NEWVERSION}/g' clang/PKGBUILD" | bash
 echo "sed -i 's/${PKGREL}/pkgrel=1/g' clang/PKGBUILD" | bash
 grep -oP "'\K[a-f0-9]{64}(?='.*)" clang/PKGBUILD | awk '{print "sed -i \x27s/"$1"/SKIP/g\x27 clang/PKGBUILD"}' | bash
 
-sed -i '/patch -Np2/d' clang/PKGBUILD 
+#sed -i '/patch -Np2/d' clang/PKGBUILD 
 # sed -i '/clang-tools-extra|clang/d' clang/PKGBUILD
 sed -i 's/-DLLVM_BUILD_TESTS=ON/-DLLVM_BUILD_TESTS=OFF/g' clang/PKGBUILD
