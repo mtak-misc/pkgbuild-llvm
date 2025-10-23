4a1c05276f9f637dd2674f007004c6144295514c
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2025-10-23 19:57:25 +0900

    Update modify-clang-pkgbuild.sh

--- modify-clang-pkgbuild.sh
+++ modify-clang-pkgbuild.sh
@@ -15,10 +15,10 @@ else
 fi
 
 #sed -i 's/gcc/clang/g' clang/PKGBUILD
-#echo "sed -i 's/${OLDVERSION}/${NEWVERSION}/g' clang/PKGBUILD" | bash
-#echo "sed -i 's/${PKGREL}/pkgrel=1/g' clang/PKGBUILD" | bash
-#grep -oP "'\K[a-f0-9]{64}(?='.*)" clang/PKGBUILD | awk '{print "sed -i \x27s/"$1"/SKIP/g\x27 clang/PKGBUILD"}' | bash
+echo "sed -i 's/${OLDVERSION}/${NEWVERSION}/g' clang/PKGBUILD" | bash
+echo "sed -i 's/${PKGREL}/pkgrel=1/g' clang/PKGBUILD" | bash
+grep -oP "'\K[a-f0-9]{64}(?='.*)" clang/PKGBUILD | awk '{print "sed -i \x27s/"$1"/SKIP/g\x27 clang/PKGBUILD"}' | bash
 
-#sed -i '/patch -Np2/d' clang/PKGBUILD 
+sed -i '/patch -Np2/d' clang/PKGBUILD 
 # sed -i '/clang-tools-extra|clang/d' clang/PKGBUILD
-#sed -i 's/-DLLVM_BUILD_TESTS=ON/-DLLVM_BUILD_TESTS=OFF/g' clang/PKGBUILD
+sed -i 's/-DLLVM_BUILD_TESTS=ON/-DLLVM_BUILD_TESTS=OFF/g' clang/PKGBUILD
