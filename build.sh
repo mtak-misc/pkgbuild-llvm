d03b16995a67cfc2629887403cf2d53a0b310d9c
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2025-11-07 05:49:44 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,15 +9,15 @@ if [ $? -ne 0 ]; then
   echo 'Unexpected error.'
 elif [ -z "$RES" ]; then
   echo 'No packages.'
-#  pacman --noconfirm -S llvm compiler-rt clang lld
+  #
+  export CC=clang
+  export CXX=clang++
+  pacman --noconfirm -S llvm compiler-rt clang lld
 else
   pacman --noconfirm -U *.pkg.tar.zst
   export CC=clang
   export CXX=clang++
 fi
-
-#export CC=clang
-#export CXX=clang++
 
 cd ./llvm ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm --nocheck --skippgpcheck -sc"
 rm llvm-debug*.zst
