377c75ef34931a931074006325f8849b0253ccba
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2025-11-07 06:30:02 +0900

    Update modify-llvm-pkgbuild.sh

--- modify-llvm-pkgbuild.sh
+++ modify-llvm-pkgbuild.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 NEWVERSION=$1
+LLVMBUILD=$2
 PKGVERSION=$(grep "pkgver=" llvm/PKGBUILD)
 OLDVERSION="${PKGVERSION#pkgver=}"
 PKGREL=$(grep "pkgrel=" llvm/PKGBUILD)
@@ -9,7 +10,10 @@ if [ $? -ne 0 ]; then
   echo 'Unexpected error.'
 elif [ -z "$RES" ]; then
   echo 'No packages.'
-#  pacman --noconfirm -S llvm compiler-rt clang lld
+#  pacman --noconfirm -S llvm compiler-rt clang lld 
+  if [ ${LLVMBUILD} = "true" ]; then
+    sed -i 's/gcc-libs/llvm-libs/g' llvm/PKGBUILD
+  fi
 else
   sed -i 's/gcc-libs/llvm-libs/g' llvm/PKGBUILD
 fi
