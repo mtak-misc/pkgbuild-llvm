f98abfd6a4519bc8ef73c504474223fc5d824987
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-03-06 22:29:34 +0900

    Update PKGBUILD for LLVM and Compiler-RT paths

--- modify-llvm-source.sh
+++ modify-llvm-source.sh
@@ -10,3 +10,13 @@ sed -i 's/cd llvm-project-$pkgver.src\/build/cd llvm-project-$pkgver.src\/llvm\/
 
 sed -i 's/\.\.\/LICENSE.TXT/..\/..\/LICENSE.TXT/g' llvm/PKGBUILD
 sed -i 's/llvm-$pkgver.src\/LICENSE.TXT/llvm-project-$pkgver.src\/llvm\/LICENSE.TXT/g' llvm/PKGBUILD
+
+sed -i '/source=(/,/)/c\source=($_source_base/llvm-project-$pkgver.src.tar.xz{,.sig})' compiler-rt/PKGBUILD
+
+sed -i '/sha256sums=(/,/)/c\sha256sums=(\'SKIP\'\n            \'SKIP\')' compiler-rt/PKGBUILD
+
+sed -i '/prepare() {/,/}/c\prepare() {\n  # llvm-project内の構成を利用\n  mkdir -p llvm-project-$pkgver.src/compiler-rt/build\n}' compiler-rt/PKGBUILD
+
+sed -i 's|cd compiler-rt-$pkgver.src/build|cd llvm-project-$pkgver.src/compiler-rt/build|' compiler-rt/PKGBUILD
+
+sed -i 's|cd compiler-rt-$pkgver.src/build|cd llvm-project-$pkgver.src/compiler-rt/build|' compiler-rt/PKGBUILD
