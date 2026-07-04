e88a21b9fb078e69d175b7ba473ee61a1b357b85
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-07-05 08:11:10 +0900

    Update modify-clang-pkgbuild.sh

--- modify-clang-pkgbuild.sh
+++ modify-clang-pkgbuild.sh
@@ -21,7 +21,7 @@ fi
 # sed -i 's/gcc/clang/g' clang/PKGBUILD
 echo "sed -i 's/${OLDVERSION}/${NEWVERSION}/g' clang/PKGBUILD" | bash
 echo "sed -i 's/${PKGREL}/pkgrel=1/g' clang/PKGBUILD" | bash
-grep -oP "'\K[a-f0-9]{64}(?='.*)" clang/PKGBUILD | awk '{print "sed -i \x27s/"$1"/SKIP/g\x27 clang/PKGBUILD"}' | bash
+grep -oP "(?<=')[a-f0-9]{64}(?=')" clang/PKGBUILD | awk '{print "sed -i \x27s/"$1"/SKIP/g\x27 clang/PKGBUILD"}' | bash
 
 # sed -i '/patch -Np2/d' clang/PKGBUILD 
 # sed -i '/clang-tools-extra|clang/d' clang/PKGBUILD
