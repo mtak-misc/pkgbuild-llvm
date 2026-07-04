6cdbb1efa6c25ac7adcdfabf0e700fea6ee09b98
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-07-05 08:11:48 +0900

    Fix regex pattern for grep command in script

--- modify-compiler-rt-pkgbuild.sh
+++ modify-compiler-rt-pkgbuild.sh
@@ -21,5 +21,5 @@ fi
 sed -i '/makedepends_x86_64/d' compiler-rt/PKGBUILD
 echo "sed -i 's/${OLDVERSION}/${NEWVERSION}/g' compiler-rt/PKGBUILD" | bash
 echo "sed -i 's/${PKGREL}/pkgrel=1/g' compiler-rt/PKGBUILD" | bash
-grep -oP "'\K[a-f0-9]{64}(?='.*)" compiler-rt/PKGBUILD | awk '{print "sed -i \x27s/"$1"/SKIP/g\x27 compiler-rt/PKGBUILD"}' | bash
+grep -oP "(?<=')[a-f0-9]{64}(?=')" compiler-rt/PKGBUILD | awk '{print "sed -i \x27s/"$1"/SKIP/g\x27 compiler-rt/PKGBUILD"}' | bash
 #sed -i 's/llvm-lib32-libs/lib32-llvm-libs/g' compiler-rt/PKGBUILD
