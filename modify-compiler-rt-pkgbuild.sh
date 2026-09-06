61ee4ef8cf37bdd696ceed75534860c93c1e7942
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-09-06 10:32:02 +0900

    Disable LLVM_ENABLE_DEBUGINFOD in PKGBUILD

--- modify-compiler-rt-pkgbuild.sh
+++ modify-compiler-rt-pkgbuild.sh
@@ -23,3 +23,4 @@ echo "sed -i 's/${OLDVERSION}/${NEWVERSION}/g' compiler-rt/PKGBUILD" | bash
 echo "sed -i 's/${PKGREL}/pkgrel=1/g' compiler-rt/PKGBUILD" | bash
 grep -oP "(?<=')[a-f0-9]{64}(?=')" compiler-rt/PKGBUILD | awk '{print "sed -i \x27s/"$1"/SKIP/g\x27 compiler-rt/PKGBUILD"}' | bash
 #sed -i 's/llvm-lib32-libs/lib32-llvm-libs/g' compiler-rt/PKGBUILD
+sed -i 's/-DCMAKE_SKIP_RPATH=ON/-DCMAKE_SKIP_RPATH=ON -DLLVM_ENABLE_DEBUGINFOD=OFF/g' compiler-rt/PKGBUILD
