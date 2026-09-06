f2458a53e3df4119ce60f82a644eba064cf612dc
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-09-06 10:14:13 +0900

    Add cat command for llvm/PKGBUILD
    Added a command to display the contents of the PKGBUILD file.

--- modify-llvm-pkgbuild.sh
+++ modify-llvm-pkgbuild.sh
@@ -13,6 +13,8 @@ sed -i \
   -e "s/'91eaec904dcc7e02cd4f5007d844e0b7cb34a2cd8b9757d8a3f329383978bb20'//g" \
   llvm/PKGBUILD  
 
+cat llvm/PKGBUID
+
 RES=`find ./ -maxdepth 1 -name "*.pkg.tar.zst" 2>/dev/null`
 if [ $? -ne 0 ]; then
   echo 'Unexpected error.'
